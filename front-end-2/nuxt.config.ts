5447ca25018d71add2e59fa52355ff179337ec22
zahra <nabiz.arjavand@gmail.com>
2024-02-28 18:27:00 +0330

    new changes

--- front-end-2/nuxt.config.ts
+++ front-end-2/nuxt.config.ts
@@ -1,6 +1,7 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 // @ts-ignore
 export default defineNuxtConfig({
+  modules: ['@pinia/nuxt' , '@nuxtjs/i18n', '@nuxtjs/i18n',],
   devtools: { enabled: true },
   postcss: {
     plugins: {
